28d36bf1ccd845d73899d72ceeadeb2a8083d7ce
mtnmr <mei.rila1013@gmail.com>
2022-04-11 13:24:59 +0900

    dagger annotation

---
+++ android-dagger/app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,20 @@
+package com.example.android.dagger.di
+
+import android.content.Context
+import com.example.android.dagger.registration.RegistrationActivity
+import dagger.BindsInstance
+import dagger.Component
+
+@Component(modules = [StorageModule::class])
+interface AppComponent {
+
+    //Storageはcontextを引数に持つ、ContextはAndroidシステムから提供される（グラフの外側で構築される）
+    //このグラフの外側で構築されるインスタンスを作る
+    @Component.Factory
+    interface Factory{
+        //@BindsInstanceでDaggerにそのインスタンスをグラフに追加する必要があることを伝える
+        fun create(@BindsInstance context: Context) : AppComponent
+    }
+
+    fun inject(activity:RegistrationActivity)
+}
