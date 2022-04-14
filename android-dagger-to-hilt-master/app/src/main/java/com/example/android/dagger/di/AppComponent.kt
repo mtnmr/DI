ab5134091aa14f5ca1ea141b9f6144a12f5066ce
mtnmr <mei.rila1013@gmail.com>
2022-04-14 19:22:53 +0900

    Migrating the Application component

--- android-dagger-to-hilt-master/app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ android-dagger-to-hilt-master/app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -24,22 +24,24 @@ import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
+//EntryPointを追加したためAppComponentは不要になった。
+
 // Scope annotation that the AppComponent uses
 // Classes annotated with @Singleton will have a unique instance in this Component
-@Singleton
-// Definition of a Dagger component that adds info from the different modules to the graph
-@Component(modules = [StorageModule::class, AppSubcomponents::class])
-interface AppComponent {
-
-    // Factory to create instances of the AppComponent
-    @Component.Factory
-    interface Factory {
-        // With @BindsInstance, the Context passed in will be available in the graph
-        fun create(@BindsInstance context: Context): AppComponent
-    }
-
-    // Types that can be retrieved from the graph
-    fun registrationComponent(): RegistrationComponent.Factory
-    fun loginComponent(): LoginComponent.Factory
-    fun userManager(): UserManager
-}
+//@Singleton
+//// Definition of a Dagger component that adds info from the different modules to the graph
+//@Component(modules = [StorageModule::class, AppSubcomponents::class])
+//interface AppComponent {
+//
+//    // Factory to create instances of the AppComponent
+//    @Component.Factory
+//    interface Factory {
+//        // With @BindsInstance, the Context passed in will be available in the graph
+//        fun create(@BindsInstance context: Context): AppComponent
+//    }
+//
+//    // Types that can be retrieved from the graph
+//    fun registrationComponent(): RegistrationComponent.Factory
+//    fun loginComponent(): LoginComponent.Factory
+//    fun userManager(): UserManager
+//}
--- android-dagger/app/src/main/java/com/example/android/dagger/di/StorageModule.kt
+++ android-dagger/app/src/main/java/com/example/android/dagger/di/StorageModule.kt
@@ -1,13 +1,25 @@
 package com.example.android.dagger.di
 
+import android.content.Context
 import com.example.android.dagger.storage.SharedPreferencesStorage
 import com.example.android.dagger.storage.Storage
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 
 @Module
 abstract class StorageModule {
     // Makes Dagger provide SharedPreferencesStorage when a Storage type is requested
     @Binds
     abstract fun provideStorage(storage: SharedPreferencesStorage): Storage
+}
+
+//@Providesを使って以下のようにも書ける
+@Module
+class StorageModuleProvide {
+
+    @Provides
+    fun provideStorage(context: Context):Storage{
+        return SharedPreferencesStorage(context)
+    }
 }
